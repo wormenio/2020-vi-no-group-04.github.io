9146d566b386843e4a4713ef9e9cc6104f3355f9
Rodrigo Rangoni <rrangoni@Randall.vm>
2020-06-16 21:21:09 -0300

    agrego validaciones de la compra, punto 3

--- src/main/java/modelo/Compra.java
+++ src/main/java/modelo/Compra.java
@@ -2,24 +2,69 @@ package modelo;
 
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 public class Compra implements ReferenciaEgreso {
+	 int maximoPresupuestosRequeridos;
+	 Criterio criterio;
 	 Proveedor proveedor;
 	 List<Presupuesto> presupuestos;
+	 Presupuesto presupuestoAsignado;
 	 List<Usuario> usuariosHabilitados;
 	 List<Item> items = new ArrayList<Item> ();
+	 //Moneda moneda;
 	 
-	 public boolean usuarioHabilitado(Usuario usuario) {
+	 public boolean esRevisor(Usuario usuario) {
 		 return true;
 	 }
 	 
-	 public void habilitarUsuario(Usuario usuario) {
+	 public void HacerRevisor(Usuario usuario) {
 		 this.usuariosHabilitados.add(usuario);
 	 }
+	 
 	 public int  calcularMonto() {
 		return 0;
 		 
 	 }
+	 
+	 public boolean validarCompra(){		 
+		 if(this.maximoPresupuestosRequeridos > 0) {			 			 
+			 return this.validoCantidadDePresupuestos()  
+					&& this.validoPresupuestoAsignadoContenidoEnElListado() 
+					&& this.validoPorCriterioDeMenorValor();
+		 }
+		 
+		 
+		 
+		 return true;
+	 }
+	 
+	 public boolean validoCantidadDePresupuestos() {
+		 return this.presupuestos.size() == this.maximoPresupuestosRequeridos;		 
+	 }
+	 
+	 public boolean validoPresupuestoAsignadoContenidoEnElListado() {
+		 return this.presupuestos.contains(this.presupuestoAsignado);		 
+	 }
+	 
+	 public boolean validoPorCriterioDeMenorValor() {
+		 
+		 boolean validoCriterioMinimoPresupuesto = false;
+		 
+		 if(this.criterio == Criterio.MENOR_VALOR) {			
+
+			    Presupuesto minPresupuesto = this.presupuestos
+			      .stream()
+			      .min(Comparator.comparing(Presupuesto::GetValor))
+			      .orElseThrow(null);
+			 				 
+			    validoCriterioMinimoPresupuesto = minPresupuesto.detalle.total == this.presupuestoAsignado.detalle.total;
+		 }
+		 
+		 return validoCriterioMinimoPresupuesto;
+		 
+	 }
+	 
 
 }
