9f911afd004be3b05dba58423e1ee8df7ac259ff
Rodrigo Rangoni <rodrigo.rangoni@gmail.com>
2020-06-21 19:49:54 -0300

    Categorizacion Segun AFIP

--- src/main/java/modelo/CategorizacionAFIP.java
+++ src/main/java/modelo/CategorizacionAFIP.java
@@ -2,6 +2,13 @@ package modelo;
 
 public enum CategorizacionAFIP {
 	MICRO,PEQUENIA,MEDIANA_TRAMO_1,MEDIANA_TRAMO_2;
+        
+    public CategorizacionAFIP getCategorizacionAFIP(int cantidadEmpleados){
+        CategorizacionAFIP categoria = 
+                (cantidadEmpleados >= 5 && cantidadEmpleados <= 15) ? CategorizacionAFIP.MICRO :
+                (cantidadEmpleados > 15 && cantidadEmpleados <= 60) ? CategorizacionAFIP.PEQUENIA :
+                (cantidadEmpleados > 60 && cantidadEmpleados <= 235) ? CategorizacionAFIP.MEDIANA_TRAMO_1 :
+                 CategorizacionAFIP.MEDIANA_TRAMO_2;
+        return categoria;
+    }
 }
-
-// Solo para empleados
