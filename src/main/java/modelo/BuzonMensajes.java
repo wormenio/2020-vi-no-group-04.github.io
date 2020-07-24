00bcbd5ff1f4d0c8c7c52e9e1fd134073ecb5862
bmangia <bmangia@localhost>
2020-07-24 19:50:38 -0300

    limpieza codigo

--- src/main/java/modelo/BuzonMensajes.java
+++ src/main/java/modelo/BuzonMensajes.java
@@ -2,15 +2,11 @@ package modelo;
 import java.util.ArrayList;
 import java.util.List;
 
-//import org.javatuples.*;
 
 public class BuzonMensajes {
 
 	
-  //  List<Pair<CompraConPresupuesto,Boolean>> listaDeCompras = new ArrayList<Pair<CompraConPresupuesto,Boolean>>();
-   
-	
-    List<String> listaNotificaciones = new ArrayList<String>();
+ 	
     RepositorioCompras repositorioCompras;
     
 	public BuzonMensajes(RepositorioCompras repositorio) {
@@ -25,31 +21,6 @@ public class BuzonMensajes {
     													});
     }
 
-/*	public void agregarNotificacion(CompraConPresupuesto compra, Boolean validacion) {
-		Pair<CompraConPresupuesto,Boolean> compraValidada = new Pair<CompraConPresupuesto,Boolean>(compra,validacion);
-	
-		listaDeCompras.add(compraValidada);
-		
-	}
 
-	
-
-	public List<String> verMensajesDeUsuario(Usuario usuario) {
-		
-		listaDeCompras.forEach((p)->
-		{if(p.getValue0().esRevisor(usuario)) {
-			String numeroCompra = String.valueOf(p.getValue0().documentoComercial.numero());
-			String valorValidez = String.valueOf(p.getValue1());
-			
-			listaNotificaciones.add(numeroCompra +' ' +  valorValidez);
-			
-			
-     	 }
-		 
-		});
-		
-		return listaNotificaciones;
-			
-	}*/
 	
 }
