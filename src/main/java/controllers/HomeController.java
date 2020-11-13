b6886e1c0f4720cf33306528c80fe8eca8f336ea
walter.ormenio@gmail.com <walter.ormenio@gmail.com>
2020-11-12 21:09:54 -0300

    inicia spark

---
+++ src/main/java/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package controllers;
+
+import spark.ModelAndView;
+
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HomeController {
+    public ModelAndView getHome() {
+        Map<String, Object> modelo = new HashMap<>();
+        modelo.put("anio", LocalDate.now().getYear());
+//        modelo.put("consultoras", RepositorioConsultoras.instancia.listar().subList(0, 3));
+
+        return new ModelAndView(modelo, "index.html.hbs");
+    }
+}
--- src/main/java/main/Routes.java
+++ src/main/java/main/Routes.java
@@ -1,7 +1,8 @@
 package main;
 
 import controllers.EgresosController;
-import jdk.nashorn.internal.runtime.linker.Bootstrap;
+import controllers.HomeController;
+import spark.ModelAndView;
 import spark.Spark;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
@@ -9,18 +10,21 @@ public class Routes {
     public static void main(String[] args) {
         System.out.println("Iniciando servidor");
 
-        Spark.port(8080);
+        Spark.port(8081);
         Spark.staticFileLocation("/public");
 
-        new Bootstrap().run();
+//        new Bootstrap().run();
 
+        new Bootstrap().run();
         HandlebarsTemplateEngine engine = new HandlebarsTemplateEngine();
         EgresosController egresosController = new EgresosController();
-//        HomeController homeController = new HomeController();
+        HomeController homeController = new HomeController();
 //        UsuariosController usuariosController = new UsuariosController();
 
 //
-//        Spark.get("/", (request, response) -> homeController.getHome(), engine);
+        Spark.get("/", (request, response) -> homeController.getHome(), engine);
+        Spark.get("/", (request,response) -> { return new ModelAndView(null,"login.html.hbs");} , engine);
+
 //
 //        Spark.get("/consultoras", consultorasController::getConsultoras, engine);
 //
